cc19f5e423a05dff39b912d23338620883c91df9
mrderwyn <91188169+mrderwyn@users.noreply.github.com>
2022-12-09 15:17:29 +0300

    Fixed sql spacing

--- SqlTask/create tables.sql
+++ SqlTask/create tables.sql
@@ -1,20 +1,20 @@
 CREATE TABLE [dbo].[Categories]
 (
 	[CategoryID]   INT           IDENTITY (1, 1) NOT NULL,
-    [CategoryName] NVARCHAR (15) NOT NULL,
+    	[CategoryName] NVARCHAR (15) NOT NULL,
 	CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([CategoryID] ASC)
 );
 
 CREATE TABLE [dbo].[Products]
 (
-	[ProductID]		INT				IDENTITY (1, 1) NOT NULL,
+	[ProductID]	INT		IDENTITY (1, 1) NOT NULL,
 	[ProductName]	NVARCHAR (40)	NOT NULL,
 	CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductID] ASC)
 );
 
 CREATE TABLE [dbo].[Products_Categories]
 (
-    [ProductID] INT NOT NULL,
+    	[ProductID] INT NOT NULL,
 	[CategoryID] INT NOT NULL, 
-    CONSTRAINT [PK_Products_Categories] PRIMARY KEY ([ProductID], [CategoryID])
+    	CONSTRAINT [PK_Products_Categories] PRIMARY KEY ([ProductID], [CategoryID])
 );
